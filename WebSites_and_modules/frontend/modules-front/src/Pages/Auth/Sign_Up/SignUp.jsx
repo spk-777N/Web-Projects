998f820bb206909b809016516064dc76e6be0c9d
Nazeer Rassad <nazeer.rassed@gmail.com>
2025-08-12 14:28:03 +0300

    update

--- WebSites_and_modules/frontend/modules-front/src/Pages/Auth/Sign_Up/SignUp.jsx
+++ WebSites_and_modules/frontend/modules-front/src/Pages/Auth/Sign_Up/SignUp.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import "./SignUp.css"
+import { React, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import "./SignUp.css"
 
 
 
@@ -10,11 +10,36 @@ function SignUp() {
 
     const navigate = useNavigate();
 
+    const [formData, setFormData] = useState({
+        "username": "",
+        "email": "",
+        "password": ""
+    }
+    );
+
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormData(prevData => ({
+            ...prevData,
+            [name]: value
+        }))
+    };
+
+
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+        console.log(formData);
+    };
+
 
     // page Body 
     return (
         <div className='sign-up-page'>
-            <div className='form'>
+
+            <form className='form' onSubmit={handleSubmit}>
                 <div className='signup-div-title'>
                     <p className='signup-title'>Sign up</p>
                     <p className='signup-description'>Create and account or <span onClick={() => navigate("/sign-in")}>Sign in</span ></p>
@@ -22,15 +47,15 @@ function SignUp() {
                 <div className='form-filds'>
                     <div className='signup-email'>
                         <p>Email address</p>
-                        <input type='text' />
+                        <input type='text' name='email' required value={formData.email} onChange={handleChange} />
                     </div>
                     <div className='signup-username'>
                         <p>Username</p>
-                        <input type='text' required />
+                        <input type='text' name='username' required value={formData.username} onChange={handleChange} />
                     </div>
                     <div className='signup-password'>
                         <p>Password</p>
-                        <input type='password' required />
+                        <input type='password' name='password' required value={formData.password} onChange={handleChange} />
                     </div>
                 </div>
                 <div className='sign-up-button-div'>
@@ -39,7 +64,7 @@ function SignUp() {
                         By signing up to create an account, you are accepting our terms of service and privacy policy
                     </p>
                 </div>
-            </div>
+            </form>
         </div>
     )
 }
